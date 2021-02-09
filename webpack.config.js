f865ff577245e4f498d6f1a033d779a771ad5d09
shangria <shangrialena@gmail.ru>
2021-02-09 23:37:42 +0200

    build

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const fs = require('fs');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+const PAGES_DIR = './src/pages';
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
+
+
+module.exports = (argv) => ({
+    entry : {
+        index: './src/js/index.js',
+    },
+    output: {
+        publicPath : './',
+        path: path.resolve(__dirname,'docs'),
+        filename: 'js/[name].js',
+        workerChunkLoading: false
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        ...PAGES.map(page => new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            chunks: ['name'],
+            template: `${PAGES_DIR}/${page}`,
+            filename: `./${page.replace(/\.pug/, '.html')}`
+        })),
+        new MiniCssExtractPlugin({
+            filename: 'css/style.css'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [ "babel-loader", "eslint-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'], 
+            },
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+            {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url:false,
+                        }
+                    },
+                    {
+                        loader: 'resolve-url-loader',
+                        options: {
+                            keepQuery: true,
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }], 
+            },
+            {
+                test: /\.pug$/,
+                use: {
+                    loader: 'pug-loader'
+                },
+            },
+            {
+            },
+        ]  
+    },
+    devtool: argv.mode === 'development' ?
+        'eval-source-map' :
+        undefined
+});
