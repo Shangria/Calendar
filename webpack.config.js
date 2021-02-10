34b79e24ea8463b96715f241c19bd414a9605d6c
shangria <shangrialena@gmail.ru>
2021-02-10 20:56:37 +0200

    add table

--- webpack.config.js
+++ webpack.config.js
@@ -74,6 +74,10 @@ module.exports = (argv) => ({
             },
         ]  
     },
+    watchOptions: {
+        aggregateTimeout: 500,
+        poll: 1000 // порверяем изменения раз в секунду
+    },
     devtool: argv.mode === 'development' ?
         'eval-source-map' :
         undefined
